a85911d3d3cec79e4fccd1506d4055059e70037b
GANitak <freemindys@gmail.com>
2022-02-13 16:09:34 +0100

    added card Skills src/Components/Skills && App.css refine

---
+++ ui-wbe/src/Components/Skills.jsx
@@ -0,0 +1,18 @@
+function Skills() {
+	return (
+    	<div className="Skills">
+    		<header className="App-skills">
+        		<h2>•Languages : JavaScript-HTML-CSS</h2>
+				<h4>•Framework: ReactJS-VueJS-Angular-Netlify for deployment
+				•Database: SQL-noSQL -Azure -RavenDB -MongoDB
+				•Agile methods</h4>
+				<h2>Web development of websites for scaling entreprises. <br/>The goal is to empower professionals with the use of <br/>~effective tools~ <br/> 
+				<code>:: Vision and Mission ::</code><br/> 
+				We seek to thrive in the digital market, reinventing ourselves to make a significant contribution to the ongoing evolution of web design and software development on an international scale.
+		  		</h2>
+		  	</header>
+		</div>
+	);
+}
+
+export default Skills;
