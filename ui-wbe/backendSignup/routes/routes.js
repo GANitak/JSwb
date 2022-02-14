236902112552065679aad9fbfb2ee70ed0129706
GANitak <freemindys@gmail.com>
2022-02-14 10:18:41 +0100

    added .env & routes/UPDATES server.js UPDATES && public/manifest.json&&favicons/&& App.css refine

--- ui-wbe/backendSignup/routes/routes.js
+++ ui-wbe/backendSignup/routes/routes.js
@@ -2,6 +2,8 @@ const express = require('express')
 const router = express.Router()
 const signUpTemplateCopy = require('../models/signUpModels')
 
+
+//Only POST request SEND BY USER :: it's a SignUP sent to server.js
 router.post('/signup', (req, res) => {
 	const signUpUser = new signUpTemplateCopy({
 		fullName:request.body.fullName,
@@ -11,11 +13,13 @@ router.post('/signup', (req, res) => {
 	})
 	signUpUser.save()
 	.then(data => {
-		res.json.(data)
+		res.json(data)
 	})
 	.catch(error => {
-		res.json.(error)
+		res.json(error)
 	})
 })
+
+
 
 module.exports = router
